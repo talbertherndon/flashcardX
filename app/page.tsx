2fa6cb3dbb9ffd6d8d7e08b6de58351944cbe375
talbert500 <56614622+talbertherndon@users.noreply.github.com>
2023-04-20 13:57:30 -0700

    fixed link

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ export default async function Home() {
         >
           <a
             className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
-            href={DEPLOY_URL}
+            href={"https://www.jotted.page/"}
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -70,7 +70,7 @@ export default async function Home() {
 
             <p>Mico-Learning Community</p>
           </a>
-          
+
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
             href="https://github.com/talbertherndon/flashcardX"
